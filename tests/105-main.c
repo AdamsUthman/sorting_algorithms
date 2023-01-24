4295a5a4a281b9227f4ebe69f6938314686c3552
Adams <106625733+AdamsUthman@users.noreply.github.com>
2023-01-24 18:35:07 +0100

    Update 105-main.c

--- tests/105-main.c
+++ tests/105-main.c
@@ -3,12 +3,19 @@
 #include "../sort.h"
 
 /**
- * main - Entr point
+ * main - Entry point
  *
- * Return: adknkdvndv
+ * Return: Always 0
  */
-
 int main(void)
 {
-	int array[] = {24, 424, 244}
+	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+	size_t n = sizeof(array) / sizeof(array[0]);
+
+	print_array(array, n);
+	printf("\n");
+	radix_sort(array, n);
+	printf("\n");
+	print_array(array, n);
+	return (0);
 }
