91693ea586ec68c980ec662d12b1edb413ed28c3
Adams <106625733+AdamsUthman@users.noreply.github.com>
2023-01-24 18:34:47 +0100

    Update 104-main.c

--- tests/104-main.c
+++ tests/104-main.c
@@ -5,10 +5,17 @@
 /**
  * main - Entry point
  *
- * eturn: jsjb
+ * Return: Always 0
  */
-
 int main(void)
 {
-	int array[] = {3, 4, 55}
+	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+	size_t n = sizeof(array) / sizeof(array[0]);
+
+	print_array(array, n);
+	printf("\n");
+	heap_sort(array, n);
+	printf("\n");
+	print_array(array, n);
+	return (0);
 }
